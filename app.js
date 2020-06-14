c043d71000c78d5db9c86f16567f746a8a29345c
bilal <bilalkk63@gmail.com>
2020-06-14 13:49:59 +0500

    practice some more chapters

--- app.js
+++ app.js
@@ -188,3 +188,201 @@
 // var totalSnacks=(expectedAge-currentAge)*12*30*snackPerDay;
 // document.write("<h1>The lifetime supply calculator</h1><br>");
 // document.write("You are going to eat "+totalSnacks+" "+snack+" in your whole expected life :) ");
+
+
+// // Chapter#06-to-09
+
+// // Task No:01
+// var a=parseInt(prompt("Enter any number:","1"));
+
+// document.write("<h3>Result</h3><br>");
+// document.write("The input number is: "+a+"<br>");
+// document.write("---------------------------------<br>");
+// document.write("The value of ++a is: "+ ++a +"<br>");
+// document.write("Now the value of a is: "+ a +"<br>");
+// document.write("---------------------------------<br>");
+// document.write("The value of a++ is: "+ a++ +"<br>");
+// document.write("Now the value of a is: "+ a +"<br>");
+// document.write("---------------------------------<br>");
+// document.write("The value of --a is: "+ --a +"<br>");
+// document.write("Now the value of a is: "+ a +"<br>");
+// document.write("---------------------------------<br>");
+// document.write("The value of a-- is: "+ a-- +"<br>");
+// document.write("Now the value of a is: "+ a +"<br>");
+
+// // Task No:02
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+// document.write(result);
+
+// // Task No:03
+// var name=prompt("What is your name?");
+// document.write("Hello "+name+", How are you?");
+
+// // Task No:04
+// var number=parseInt(prompt("Enter any number to see its table:","5"));
+// document.write("<h2>Multiplication Table of "+number+"</h2><br>");
+// document.write(number+" * 1 = "+number*1+"<br>");
+// document.write(number+" * 2 = "+number*2+"<br>");
+// document.write(number+" * 3 = "+number*3+"<br>");
+// document.write(number+" * 4 = "+number*4+"<br>");
+// document.write(number+" * 5 = "+number*5+"<br>");
+// document.write(number+" * 6 = "+number*6+"<br>");
+// document.write(number+" * 7 = "+number*7+"<br>");
+// document.write(number+" * 8 = "+number*8+"<br>");
+// document.write(number+" * 9 = "+number*9+"<br>");
+// document.write(number+" * 10 = "+number*10+"<br>");
+
+// // Task No:05
+// var sub1=prompt("Enter first subject name:");
+// var mks1=parseInt(prompt("Enter obtained marks in "+sub1));
+// var sub2=prompt("Enter second subject name:");
+// var mks2=parseInt(prompt("Enter obtained marks in "+sub2));
+// var sub3=prompt("Enter third subject name:");
+// var mks3=parseInt(prompt("Enter obtained marks in "+sub3));
+// var totalMarks=100;
+// document.write("<h2>Subject----Total Marks----Obtained Marks----Percentage</h2><br>");
+// document.write("<h2>"+sub1+"----"+totalMarks+"----"+mks1+"----"+mks1/totalMarks*100+"%</h2><br>");
+// document.write("<h2>"+sub2+"----"+totalMarks+"----"+mks2+"----"+mks2/totalMarks*100+"%</h2><br>");
+// document.write("<h2>"+sub3+"----"+totalMarks+"----"+mks3+"----"+mks3/totalMarks*100+"%</h2><br>");
+// document.write("<h2>"+totalMarks*3+"----"+(mks1+mks2+mks3)+"</h2><br>");
+
+
+// // Chapter#09-to-11
+
+// //Task No:01
+// var city=prompt("Enter your city","karachi");
+// if(city=="karachi"){
+// document.write("Welcome to the city of lights");
+// }else if(city=="mirpurkhas"){
+//     document.write("Welcome to the city of mangoes");
+// }else if(city=="sargodha"){
+//     document.write("Welcome to the city of eagles");
+// }else{
+//     document.write("Sorry, your city is not in our database.");
+// }
+
+// //Task No:02
+// var gender=prompt("What is your gender");
+// if(gender=="male"){
+//     document.write("Hello Sir, how are you?");
+//     }else if(gender=="female"){
+//         document.write("Hello Mam, how are you?s");
+//     }else{
+//         document.write("Sorry, gender not specified.");
+//     }
+
+// //Task No:03
+// var trafficColor=prompt("Name the traffic signal color:");
+// if(trafficColor==="red"){
+// document.write("Must Stop!");
+// }else if(trafficColor==="yellow"){
+//     document.write("Ready to move!");
+// }else if(trafficColor==="green"){
+//     document.write("Move now!");
+// }else{
+//     document.write("Sorry, That's not traffic color!");
+// }
+
+// //Task No:04
+// var currentFuel=prompt("How much fuel is remaining?");
+// if(currentFuel<0.25){
+//     document.write("Please refill the tank");
+// }else{
+//     document.write("You are good to go!");
+// }
+
+// //Task No:05
+
+// //a
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+// //b
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+// //c
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+// //d
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+// //e
+// if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+
+// //f
+// if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+
+//Task No:06
+// var guessedNumber=parseInt(prompt("Guess the number:"));
+// var number=4;
+// if(guessedNumber==number){
+//         document.write("Bingo! Correct answer");
+//     }else if(guessedNumber==number+1){
+//         document.write("Close enough to correct answer!");
+//     }else{
+//         document.write("Wrong guess!");
+// }
+
+// //Task No:07
+// var number=parseInt(prompt("Enter number to check wheather it is even or odd:"));
+// if(number%2==0){
+//     document.write(number+" is an even number!");
+// }else{
+//     document.write(number+" Is an odd number");
+// }
+
+// //Task No:08
+// var temp=prompt("Enter your current temperature");
+// if(temp>=40){
+//     document.write("Its too hot outside");
+// }else if(temp>=30){
+//     document.write("Its normal weather outside");
+// }else if(temp>=20){
+//     document.write("Its a cold weather");
+// }else if(temp>=10){
+//     document.write("Its too cool outside");
+// }
+
+// //Task No:09
+// var firstNum=parseInt(prompt("Enter first value:"));
+// var secondNum=parseInt(prompt("Enter second value:"));
+// var sign=prompt("Enter any of these signs +, -, *, /:");
+// if(sign == '+'){
+//     document.write("The addition is "+(firstNum+secondNum));
+// }else if(sign == '-'){
+//     document.write("The substraction is "+(firstNum-secondNum));
+// }else if(sign == '*'){
+//     document.write("The multiplication is "+(firstNum*secondNum));
+// }else if(sign == '/'){
+//     document.write("The division is "+(firstNum/secondNum));
+// }
